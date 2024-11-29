2b5c99ff31109ac6ee9ec978cbc8f0fa39f9bdf5
huutamisme <hohuutam287@gmail.com>
2024-11-30 01:26:13 +0700

    Add productRoutes for sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,7 @@
 import type { MetadataRoute } from 'next'
 import { convertToSlug } from './lib/Convertor';
 import { ProductType } from '@/components/ProductType';
+import { Products } from './san-pham/ProductData';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap>{
   const staticRoutes: MetadataRoute.Sitemap = [
@@ -36,46 +37,17 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap>{
     },
   ]
 
-  const productTypeRoute: MetadataRoute.Sitemap = ProductType.map((type) => ({
+  const productTypeRoutes: MetadataRoute.Sitemap = ProductType.map((type) => ({
     url: `http://sifa999.vercel.app/san-pham/${convertToSlug(type)}`,
     lastModified: new Date(), 
-    changeFrequency: 'monthly',
+    changeFrequency: 'yearly',
   }));
 
-  return[...staticRoutes, ...productTypeRoute];
+  const productRoutes: MetadataRoute.Sitemap = Products.map((product) => ({
+    url: `http://sifa999.vercel.app/san-pham/${convertToSlug(product.type)}/${convertToSlug(product.name)}`,
+    lastModified: new Date(), 
+    changeFrequency: 'yearly',
+  }));
+
+  return[...staticRoutes, ...productTypeRoutes, ...productRoutes];
 }
- 
-// export default function sitemap(): MetadataRoute.Sitemap {
-//   return [
-//     {
-//       url: 'http://sifa999.vercel.app',
-//       lastModified: new Date(),
-//       changeFrequency: 'yearly',
-//     },
-//     {
-//       url: 'http://sifa999.vercel.app/gioi-thieu',
-//       lastModified: new Date(),
-//       changeFrequency: 'yearly',
-//     },
-//     {
-//       url: 'http://sifa999.vercel.app/san-pham',
-//       lastModified: new Date(),
-//       changeFrequency: 'yearly',
-//     },
-//     {
-//       url: 'http://sifa999.vercel.app/nha-phan-phoi',
-//       lastModified: new Date(),
-//       changeFrequency: 'yearly',
-//     },
-//     {
-//       url: 'http://sifa999.vercel.app/tuyen-dung',
-//       lastModified: new Date(),
-//       changeFrequency: 'yearly',
-//     },
-//     {
-//       url: 'http://sifa999.vercel.app/lien-he',
-//       lastModified: new Date(),
-//       changeFrequency: 'yearly',
-//     },
-//   ]
-// }
