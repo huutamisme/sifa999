fe4d3dce9f2f0bea92c14931b34659dbb2a7d4a3
huutamisme <hohuutam287@gmail.com>
2024-11-30 01:10:01 +0700

    Add dynamic routes for sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,9 @@
 import type { MetadataRoute } from 'next'
- 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
+import { convertToSlug } from './lib/Convertor';
+import { ProductType } from '@/components/ProductType';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap>{
+  const staticRoutes: MetadataRoute.Sitemap = [
     {
       url: 'http://sifa999.vercel.app',
       lastModified: new Date(),
@@ -33,4 +35,47 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'yearly',
     },
   ]
+
+  const productTypeRoute: MetadataRoute.Sitemap = ProductType.map((type) => ({
+    url: `http://sifa999.vercel.app/san-pham/${convertToSlug(type)}`,
+    lastModified: new Date(), 
+    changeFrequency: 'monthly',
+  }));
+
+  return[...staticRoutes, ...productTypeRoute];
 }
+ 
+// export default function sitemap(): MetadataRoute.Sitemap {
+//   return [
+//     {
+//       url: 'http://sifa999.vercel.app',
+//       lastModified: new Date(),
+//       changeFrequency: 'yearly',
+//     },
+//     {
+//       url: 'http://sifa999.vercel.app/gioi-thieu',
+//       lastModified: new Date(),
+//       changeFrequency: 'yearly',
+//     },
+//     {
+//       url: 'http://sifa999.vercel.app/san-pham',
+//       lastModified: new Date(),
+//       changeFrequency: 'yearly',
+//     },
+//     {
+//       url: 'http://sifa999.vercel.app/nha-phan-phoi',
+//       lastModified: new Date(),
+//       changeFrequency: 'yearly',
+//     },
+//     {
+//       url: 'http://sifa999.vercel.app/tuyen-dung',
+//       lastModified: new Date(),
+//       changeFrequency: 'yearly',
+//     },
+//     {
+//       url: 'http://sifa999.vercel.app/lien-he',
+//       lastModified: new Date(),
+//       changeFrequency: 'yearly',
+//     },
+//   ]
+// }
---
+++ components/ProductType.ts
@@ -0,0 +1,11 @@
+export const ProductType: string[] = [
+    "Chất Thông Cống, WC",
+    "Khử Mùi",
+    "Chất Tẩy Rửa",
+    "Nước Giặt, Ủi Quần Áo", 
+    "Dầu Làm Bóng", 
+    "Nước Rửa Chén", 
+    "Sản Phẩm Cho Oto", 
+    "Men Vi Sinh", 
+    "Nước Xịt Phòng", 
+]
